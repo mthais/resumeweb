701896806c8ee0a9c5766960dfe7df3335a9d4f6
Agathaemm <agathaemm@gmail.com>
2018-09-13 15:07:59 -0300

    Primeiro commit

---
+++ server/server.config.js
@@ -0,0 +1,14 @@
+/**
+ * Basic Express Server Configuration
+ *
+ */
+module.exports = {
+    
+    /**
+     * Port on express server is running
+     *
+     */
+    PORT: 3000
+};
+
+// End of file
