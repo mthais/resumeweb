215e013681211745b5f71271a22e3d1263a45c7d
Matheus Valeriano <math.valeriano@gmail.com>
2018-12-06 20:51:55 -0200

    remove unnecessary imports

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { PasswordRule } from './../../Rules/PasswordRule';
 import { FormInput } from './../../Components/Form/FormInput';
 import { LinkedinButton } from './../../Components/Social/Linkedin';
-import { Card, Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Row, Col, Button, FormGroup, Label } from 'reactstrap';
 
 export class LoginPage extends Component {
 
