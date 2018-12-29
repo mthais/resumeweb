2729ecc1034cd17d5ac15ec01f34773684594e8e
Matheus Valeriano <math.valeriano@gmail.com>
2018-12-29 11:24:48 -0200

    feat: create a wrapper to sessionroutes
    this wrapper will be used to create a card layout to sessionroutespages

--- src/Router/sessionRoutes.js
+++ src/Router/sessionRoutes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router'
+import { Row, Col, Card, CardBody, Container } from 'reactstrap'
 import { LoginPage } from '../Pages/Login'
 import { SignupPage } from '../Pages/Signup'
 import { RecoveryPasswordPage } from '../Pages/Password/recovery'
@@ -24,11 +25,30 @@ const sessionRoutes = [
   }
 ]
 
+const Wrapper = ({ children }) => (
+  <Container>
+    <Row>
+      <Col xs='12' md={ { size: 8, offset: 2 } } className='pt-5 card-session-layout'>
+        <Card>
+          <CardBody>
+            <Row>
+              <Col className='d-none d-sm-block pt-4'>
+                <img width='100%' src='/login_person.png' />
+              </Col>
+              { children }
+            </Row>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
+)
+
 const sessionRouteLayout = ({ component: Component, path }) => (
   <Route
     exact
     path={ path }
-    component={ Component }
+    render={ () => <Wrapper><Component /></Wrapper> }
     key={ path }
   />
 )
