ed011299840fc1367567405a69e069bcadb92966
Matheus Valeriano <math.valeriano@gmail.com>
2018-12-29 11:25:24 -0200

    refactor: remove unnecessary code

--- src/Pages/Password/reset.js
+++ src/Pages/Password/reset.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Formsy from 'formsy-react-2'
-import { Row, Col, Button, FormGroup, Label, Card, CardBody, Container } from 'reactstrap'
+import { Col, Button, FormGroup, Label } from 'reactstrap'
 import { FormInput } from '../../Components/Form/FormInput'
 import { PasswordRule } from './../../Rules/PasswordRule'
 
@@ -40,66 +40,51 @@ export class ResetPasswordPage extends Component {
     const { canSubmit } = this.state
 
     return (
-      <Container>
-        <Row>
-          <Col xs='12' md={ { size: 8, offset: 2 } } className='pt-5'>
-            <Card>
-              <CardBody>
-                <Row>
-                  <Col className='d-none d-sm-block pt-4'>
-                    <img width='100%' src='/login_person.png' />
-                  </Col>
-                  <Col>
-                    <h2 className='text-center page-title'>
-                                            Redefinir Senha
-                    </h2>
-                    <p className='text-wrapper'>
-                                            Digite abaixo sua nova senha!
-                    </p>
+      <Col>
+        <h2 className='text-center page-title'>
+                                Redefinir Senha
+        </h2>
+        <p className='text-wrapper'>
+                                Digite abaixo sua nova senha!
+        </p>
 
-                    <Formsy.Form onValid={ this.enableButton } onInvalid={ this.disableButton }>
-                      <FormGroup>
-                        <Label for='password' className='mb-0'>Nova Senha</Label>
-                        <FormInput
-                          ref={ this.passwordField }
-                          type='password'
-                          name='password'
-                          validations={ { passwordRule: PasswordRule } }
-                          id='password'
-                          className='form-control'
-                        />
-                      </FormGroup>
+        <Formsy.Form onValid={ this.enableButton } onInvalid={ this.disableButton }>
+          <FormGroup>
+            <Label for='password' className='mb-0'>Nova Senha</Label>
+            <FormInput
+              ref={ this.passwordField }
+              type='password'
+              name='password'
+              validations={ { passwordRule: PasswordRule } }
+              id='password'
+              className='form-control'
+            />
+          </FormGroup>
 
-                      <FormGroup>
-                        <Label for='password' className='mb-0'>Confirmar Senha</Label>
-                        <FormInput type='password'
-                          name='confirmPassword'
-                          validations={ {
-                            passwordRule: PasswordRule,
-                            equalsField: 'password'
-                          } }
-                          validationErrors={ {
-                            equalsField: 'A senha de confirmação precisa ser igual a senha atual.'
-                          } }
-                          id='confirmPassword'
-                          className='form-control' />
+          <FormGroup>
+            <Label for='password' className='mb-0'>Confirmar Senha</Label>
+            <FormInput type='password'
+              name='confirmPassword'
+              validations={ {
+                passwordRule: PasswordRule,
+                equalsField: 'password'
+              } }
+              validationErrors={ {
+                equalsField: 'A senha de confirmação precisa ser igual a senha atual.'
+              } }
+              id='confirmPassword'
+              className='form-control' />
 
-                      </FormGroup>
-                      <Button
-                        className='px-4 float-right'
-                        color='primary'
-                        disabled={ !canSubmit }
-                      >
-                                                Redefinir Senha
-                      </Button>
-                    </Formsy.Form>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+          </FormGroup>
+          <Button
+            className='px-4 float-right'
+            color='primary'
+            disabled={ !canSubmit }
+          >
+                                    Redefinir Senha
+          </Button>
+        </Formsy.Form>
+      </Col>
     )
   }
 }
