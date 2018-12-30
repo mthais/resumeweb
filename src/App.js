b85171aeda118944950a39ddc83bade52d8c544a
Matheus Valeriano <math.valeriano@gmail.com>
2018-12-30 16:13:20 -0200

    feat: includes redux provider to app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import store from './Store'
 import Router from './Router'
 
-const App = () => <Router />
+const App = () => (
+  <Provider store={ store }>
+    <Router />
+  </Provider>
+)
 
 export default App
