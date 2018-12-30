58889f0ecc4b540d34429e7830f334fe3bc84b73
Matheus Valeriano <math.valeriano@gmail.com>
2018-12-30 09:32:36 -0200

    refactor: unnecessary class
    when component doesnt manipulate any state its doesnt need to be a class

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Fragment } from 'react'
 import TopNav from './../../Components/TopNav'
 import SideNav from './../../Components/SideNav'
 import Footer from './../../Components/Footer'
@@ -8,24 +8,20 @@ import './../../assets/css/dashboard.css'
 import './../../assets/css/TopNav.css'
 import './../../assets/css/header.css'
 
-class Dashboard extends Component {
-  render () {
-    return (
-      <div>
-        <Header />
-        <br />
-        <div className='Dashboard'>
-          <TopNav />
-          <div className='curriculo'>
-            <Button className='button'>Criar novo Curriculo</Button>
-          </div>
-          <br />
-          <SideNav />
-          <Footer />
-        </div>
+const Dashboard = () => (
+  <Fragment>
+    <Header />
+    <br />
+    <div className='Dashboard'>
+      <TopNav />
+      <div className='curriculo'>
+        <Button className='button'>Criar novo Curriculo</Button>
       </div>
-    )
-  }
-}
+      <br />
+      <SideNav />
+      <Footer />
+    </div>
+  </Fragment>
+)
 
 export default Dashboard
